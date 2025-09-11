a61916ff16d615353aabeedbf0445d6f551d88ad
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-09-11 14:22:04 +0300

    кінець лекції №2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, edit or exit: ")
     user_action = user_action.strip().lower()
 
     match user_action:
@@ -10,7 +10,15 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case "show":
-            print(todos)
+            for index, item in enumerate(todos):
+                row = f"{index}--{item}"
+                print(row)
+        case "edit":
+            number = int(input("Number of the todo to edit: "))
+            number = number - 1
+            existing_todo = todos[number]
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case "exit":
             break
         case _:
