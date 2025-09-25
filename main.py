2a87961281a9d7ac28876ef46bb4d139530c877c
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-09-25 14:21:34 +0300

    Кінець лекції №4

--- main.py
+++ main.py
@@ -1,80 +1,65 @@
+def get_todos(filepath="data/todos.txt"):
+    with open(filepath, "r") as f:
+        todos_local = f.readlines()
+    return todos_local
+
 
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.strip().lower()
-
-    match user_action:
-        case "add":
-            todo = input("Enter a todo: ") + "\n"
-
-            # file = open("todos.txt", "r")
-            # todos = file.readlines()
-            # file.close()
-
-            # контекстний мененджер
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-
-            todos.append(todo)
-
-            # file=open("todos.txt",'w')
-            # file.writelines(todos)
-            # file.close()
-
-            with open("todos.txt",'w') as file:
-                file.writelines(todos)
-
-        case "show":
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
-
-            # 1-й спосіб
-            # new_todos = []
-            # for item in todos:
-            #     new_item = item.strip('\n')
-            #     new_todos.append(new_item)
-
-            # 2-й спосіб
-            # new_todos = [item.strip('\n') for item in todos]
+    user_action = user_action.strip()
 
 
-            # print(todos)
-            for index, item in enumerate(todos):
-                row = f"{index+1}--{item.strip('\n')}"
-                print(row)
-        case "edit":
-            number = int(input("Number of the todo to edit: "))
+    if user_action.lower().startswith('add'):
+        todo = user_action[4:] + "\n"
+
+        todos = get_todos("data/todos.txt")
+
+        todos.append(todo)
+
+        with open("data/todos.txt", 'w') as file:
+            file.writelines(todos)
+
+    elif user_action.lower().startswith('show'):
+        todos = get_todos()
+
+        for index, item in enumerate(todos):
+            row = f"{index+1}--{item.strip('\n')}"
+            print(row)
+    elif user_action.lower().startswith('edit'):
+        try:
+            number = int(user_action[5:])
             number = number - 1
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
 
-            # print('Here is todos existing', todos)
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            # print('Will be todos', todos)
-
-            with open("todos.txt",'w') as file:
+            with open("data/todos.txt", 'w') as file:
                 file.writelines(todos)
+        except ValueError:
+            print("Ваша команда не зовсім зрозуміла")
+            continue
 
-        case "complete":
-            number = int(input("Number of the todo to complete: "))
 
-            with open("todos.txt", "r") as file:
-                todos = file.readlines()
+    elif user_action.lower().startswith('complete'):
+        number = int(user_action[9:])
 
-            completed_todo = todos.pop(number - 1)
+        todos = get_todos()
 
-            with open("todos.txt",'w') as file:
-                file.writelines(todos)
+        completed_todo = todos.pop(number - 1)
 
-            message = f"\tТудушка \"{completed_todo.strip('\n')}\" була успішно виконана!"
-            print(message)
-        case "exit":
-            break
-        case _:
-            print("Invalid input")
+        with open("data/todos.txt", 'w') as file:
+            file.writelines(todos)
+
+        message = f"\tТудушка \"{completed_todo.strip('\n')}\" була успішно виконана!"
+        print(message)
+    elif user_action.lower().startswith('exit'):
+        break
+    else:
+        print('invalid input')
+
+    print( ' Успішне виконня команди')
 
 
 
