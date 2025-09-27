379133eea7b652029ff5dad5cadb5d90dbcab70f
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-09-27 14:23:04 +0300

    Кінець лекції №5

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def get_todos(filepath="data/todos.txt"):
-    with open(filepath, "r") as f:
-        todos_local = f.readlines()
-    return todos_local
+# from functions import get_todos, write_todos
+import time
+import functions
 
 
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print(now)
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
@@ -12,15 +13,14 @@ while True:
     if user_action.lower().startswith('add'):
         todo = user_action[4:] + "\n"
 
-        todos = get_todos("data/todos.txt")
+        todos = functions.get_todos()
 
         todos.append(todo)
 
-        with open("data/todos.txt", 'w') as file:
-            file.writelines(todos)
+        functions.write_todos(todos)
 
     elif user_action.lower().startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             row = f"{index+1}--{item.strip('\n')}"
@@ -30,13 +30,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            with open("data/todos.txt", 'w') as file:
-                file.writelines(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Ваша команда не зовсім зрозуміла")
             continue
@@ -45,12 +44,11 @@ while True:
     elif user_action.lower().startswith('complete'):
         number = int(user_action[9:])
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         completed_todo = todos.pop(number - 1)
 
-        with open("data/todos.txt", 'w') as file:
-            file.writelines(todos)
+        functions.write_todos(todos)
 
         message = f"\tТудушка \"{completed_todo.strip('\n')}\" була успішно виконана!"
         print(message)
