f88c8e70549342370dd9cccd674c1396e28b3431
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-10-16 14:42:16 +0300

    Кінець лекції №8

--- gui.py
+++ gui.py
@@ -1,25 +1,30 @@
 import functions
 import FreeSimpleGUI as sg
+import time
+
+
+sg.theme('LightBlue6')
+clock = sg.Text('',key='clock')
 
 label = sg.Text('Type a todo')
 input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("Add")
+add_button = sg.Button(size=10, image_source="add1.png", tooltip="Add new todo", key='Add', mouseover_colors="LightBlue2")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos', enable_events=True, size=[45, 12])
 
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My TODO App", layout=[
+    [clock],
     [label],
     [input_box, add_button],
-    [list_box, edit_button]
+    [list_box, edit_button, complete_button],
+    [exit_button]
 ], font=("Helvetica", 20))
 
 while True:
-    event, values = window.read()
-
-
-    print(event)
-    print(values)
+    event, values = window.read(timeout=200)
 
 
     match event:
@@ -28,13 +33,43 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
         case 'Edit':
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + '\n'
 
-            print(todo_to_edit, new_todo)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Оберіть тудушку", font=("Helvetica", 20))
+        case 'todos':
+            print(values['todos'][0])
+            window['todo'].update(value=values['todos'][0])
+        case 'Complete':
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Оберіть тудушку для завершення", font=("Helvetica", 20))
+
+        case 'Exit' :
+            break
+
         case sg.WIN_CLOSED:
             break
+
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
 
 
 window.close()
