7742f85d8d2c419b97bb5d169cf5f827edec4b5e
Volodymyr-Kovdrysh <v.kovdrysh@chnu.edu.ua>
2025-11-22 14:25:24 +0200

    Кінець лекції 14

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask('Website')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/api/v1/<station>/<date>')
+def about(station, date):
+    temperature = 12
+    return {
+        'station': station,
+        'date': date,
+        'temperature': temperature
+    }
+
+
+app.run(debug=True)
